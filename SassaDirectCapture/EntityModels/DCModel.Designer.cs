6f60fb53698bf3f15545a8a54be7aedf19e0642e
Johan Smit <endstreet@gmail.com>
2020-06-26 09:55:52 +0200

    >net 4.6.1

--- SassaDirectCapture/EntityModels/DCModel.Designer.cs
+++ SassaDirectCapture/EntityModels/DCModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'G:\Source\NewBrm\SASSADirectCapture\EntityModels\DCModel.edmx'. 
+﻿// T4 code generation is enabled for model 'G:\Source\NewBrm\SassaDirectCapture\EntityModels\DCModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
